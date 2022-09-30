90a81572abb8c348b6cfda022b406f418c897324
Emily Baird <emilym.baird@gmail.com>
2022-09-30 20:09:15 +0100

    test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 
 public class Application {
+//    note
 
     public static void main(String[] args) {
         SpeakerService service = new SpeakerServiceImpl();
