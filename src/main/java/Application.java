b5bd7d71ba8dbb1dc42e30d3e93311a3c0de8be1
Emily Baird <emilym.baird@gmail.com>
2022-10-01 17:08:34 +0100

    implement spring with java, replacing setters and constructors

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,16 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 //    note 2
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//        SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
